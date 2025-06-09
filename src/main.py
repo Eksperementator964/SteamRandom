4d9feb0a2fee344c2b9a92ae2eb133792b1add31
Eksperementator964 <eksperementator964@gmail.com>
2025-06-09 15:28:30 +0300

    Added main.py with initial code

---
+++ src/main.py
@@ -0,0 +1,47 @@
+import os
+import random
+def start_game(name, directory):
+    name_no_space = name.replace(' ', '')
+    path = directory + '\\' + name + '\\' + name + '.exe'
+    path_no_space = directory + '\\' + name + '\\' + name_no_space + '.exe'
+    if os.path.exists(path):
+        print(name)
+        # os.startfile(path)
+        return False
+    elif os.path.exists(path_no_space):
+        print(name)
+        # os.startfile(path_no_space)
+        return False
+    return True
+
+directory_D = 'D:\\SteamLibrary\\steamapps\\common'
+directory_C = "C:\\Program Files (x86)\\Steam\\steamapps\\common"
+directory_E = "E:\\SteamLibrary\\steamapps\\common"
+path = ' '
+x = True
+if os.path.exists(directory_C):
+    folders_C = [entry.name for entry in os.scandir(directory_C) if entry.is_dir()]
+else:
+    folders_C =[]
+
+if os.path.exists(directory_D):
+    folders_D = [entry.name for entry in os.scandir(directory_D) if entry.is_dir()]
+else:
+    folders_D =[]
+if os.path.exists(directory_E):
+    folders_E = [entry.name for entry in os.scandir(directory_E) if entry.is_dir()]
+else:
+    folders_E =[]
+
+folders_All = folders_C + folders_D + folders_E
+
+while x:
+    random_game_number = random.randint(0, len(folders_All) - 1)
+    if random_game_number < len(folders_C):
+        x = start_game(folders_C[random_game_number],directory_C)
+
+    elif random_game_number - len(folders_C) < len(folders_D):
+        x = start_game(folders_D[random_game_number - len(folders_C)], directory_D)
+
+    else:
+        x = start_game(folders_E[random_game_number - len(folders_C) - len(folders_D)], directory_E)
